6f65e7f7f84852ff35d04576b0bdc416e0e7dcb5
Esther Dama <esther.dama@gmail.com>
2016-06-14 23:04:54 +0300

    Adding new function to Read JSON data on Spec Runner

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,51 +1,159 @@
+// describe('Index', function() {
+//   var index = new Index();
+//   describe('Read book data', function() {
+//     it('should check .......', function() {
+//       expect(index.createIndex()).toBe("Invalid Arguement")
+      
+//     });
+
+//     it('should.....', function() {
+      
+//     });
+//   });
+  
+
+//   describe
+// });
+
+
+
+
+
+
+
+
 describe("Index", function() 
 {
-  describe("")
+  
+    // beforeEach(function(){
   var index = new Index();
-  describe("createIndex", function() 
-  {
-    it("should define {} if no create Index was called");
-    it("should create the Inverted Index from the Json file passed", function () {
-      // expect(Index.createIndex("")).not.toBe(false);//returns false when empty or no .json file passsed
-      expect(index.createIndex("jiii")).toBe(true);//returns true when it creates an index.
 
+    // });
+  describe('Read book data', function() {
+    it('should check if the file passed is a valid JSON and not empty', function() {
+      //Need to think about this
+      // reads the JSON file and asserts that it is not empty
+      // expect(index.createIndex('/books.json')).toThrow();
+      // expect(index.createIndex('/books.json')).toThrow(new Error('Cannot Find /books.json'));
+      // expect(index.createIndex('./books.json')).toBe('');
     });
-
-  });
-
-  describe("getIndex", function() 
-  {
-    it("should return the Inverted Index created by the create Index function.", function () 
-    {
-      // expect(Index.getIndex()).toBe(false);//returns false when this is run before the createIndex method //// need to find text that does this
-      expect(index.getIndex()).toBe("two"); //returns true when theis is run after the create index method.
-    });
-
   });
 
 
-  describe("searchIndex", function() 
-  {
+  describe('Populate Index', function() {
+
+    it('should assert that the Index is created after the json is reaad', function() {
+      expect(index.getIndex()).toBe("No Index Created");
+      expect(index.createIndex("http://localhost:8000/books.json")).toBeTruthy;
+      //a test that verifies that the index is created once the JSON file has been read.
+    });
+
+    it('should verify that the right word is put in the right place', function() {
+      // a test that verifies the index maps the string keys to the correct objects in the JSON array
+    });
+
+    it("should return -No Index Created- the Inverted Index created by the create Index function.", function (){
+      //     // expect(Index.getIndex()).toBe(false);//returns false when this is run before the createIndex method //// need to find text that does this
+      //     expect(index.getIndex()).toBe("No Index Created"); //returns true when theis is run after the create index method.
+    });
+  });
+
+  describe('Search index', function() {
+    beforeEach(function (done) {
+      index.createIndex("http://localhost:8000/books.json").then(function (data) {
+        done();
+      });
+    });
+
+    it("should read the JSON file and assert its not empty", function(done){
+        index.loadingData("http://localhost:8000/books.json").then(function(data){
+            //console.log("in test ", data);
+            expect(data).toBeTruthy();
+            done();
+        });
+    });
+    it('should ensure returns an array', function() {
+      //test that verifies that searching the index returns an array of the indices of the correct objects that contain the words in the search query.
+    });
     it("should return -Invalid Search Term- if not a string passed.", function () 
-    {
-      //returns false when this is run before the createIndex method //// need to find text that does this
-      expect(index.searchIndex(1)).toBe("Invalid Search Term");
-    });
+     {
+       //returns false when this is run before the createIndex method //// need to find text that does this
+       expect(index.searchIndex(1)).toBe("Invalid Search Term");
+     });
 
     it("should return -No such word found- if a string not in the books was passed.", function () 
     {
       expect(index.searchIndex("searchterm")).toBe("No such word found");
-
     });
 
-    // it("should confirm index object have properties which are the words", function () {
-    //   expect(index.searchIndex("alice")).toBe(['0']);
-    // });
+    it("should confirm index object have properties which are the words", function () {
+      expect(index.searchIndex("alice")).toEqual([0]);
+    });
 
-    // // it("should return an array of where the word is found without mindin the case", function(){
-    // //   expect(index.searchIndex("ALICE")).toBe(['0'])
-    // // });
-
+    it("should return an array of where the word is found without mindin the case", function(){
+      expect(index.searchIndex("ALICE")).toEqual([0])
+    });
   });
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  describe("createIndex", function() 
+  // {
+  //   // it("should define {} if no create Index was called");
+  //   // it("should create the Inverted Index from the Json file passed", function () {
+  //   //   // expect(Index.createIndex("")).not.toBe(false);//returns false when empty or no .json file passsed
+  //   //   expect(index.createIndex("jiii")).toBe(true);//returns true when it creates an index.
+
+  //   // });
+
+  // });
+
+  // describe("getIndex", function() 
+  // {
+  //   index.getIndex()
+  //   it("should return -No Index Created- the Inverted Index created by the create Index function.", function () 
+  //   {
+  //     // expect(Index.getIndex()).toBe(false);//returns false when this is run before the createIndex method //// need to find text that does this
+  //     expect(index.getIndex()).toBe("No Index Created"); //returns true when theis is run after the create index method.
+  //   });
+
+  // });
+
+
+  // describe("searchIndex", function() 
+  // {
+
+  //   it("should return -Invalid Search Term- if not a string passed.", function () 
+  //   {
+  //     //returns false when this is run before the createIndex method //// need to find text that does this
+  //     expect(index.searchIndex(1)).toBe("Invalid Search Term");
+  //   });
+
+  //   it("should return -No such word found- if a string not in the books was passed.", function () 
+  //   {
+  //     expect(index.searchIndex("searchterm")).toBe("No such word found");
+
+  //   });
+
+
+  //   it("should confirm index object have properties which are the words", function () {
+  //     expect(index.searchIndex("alice")).toBe([0]);
+  //   });
+
+  //   it("should return an array of where the word is found without mindin the case", function(){
+  //     expect(index.searchIndex("ALICE")).toBe([0])
+  //   });
+
+  // });
+
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,28 +1,19 @@
+var each = Function.prototype.call.bind([].forEach);
 function Index () {
 	//A private variable that will store the Inverted Index
 	var inverted_index = {};
 
 	//Dictonary of Stop words
-	var stop_words = {"a"   : true, 
-					  "i"   : true,
-					  "an"  : true, 
-					  "and" : true, 
-					  "as"  : true, 
-					  "at"  : true, 
-					  "by"  : true, 
-					  "for" : true, 
-					  "has" : true, 
-					  "in"  : true, 
-					  "is"  : true, 
-					  "it"  : true, 
-					  "of"  : true, 
-					  "on"  : true, 
-					  "that": true, 
-					  "the" : true, 
-					  "to"  : true,
-					  "was" : true, 
-					  "were": true, 
-					  "with": true};
+	var stop_words = {"a"   : true,   "i"   : true,
+					  "an"  : true,   "and" : true, 
+					  "as"  : true,   "at"  : true, 
+					  "by"  : true,   "for" : true, 
+					  "has" : true,   "in"  : true, 
+					  "is"  : true,   "it"  : true, 
+					  "of"  : true,   "on"  : true, 
+					  "that": true,   "the" : true, 
+					  "to"  : true,   "was" : true, 
+					  "were": true,   "with": true};
 
 	//Function that checks if the Inverted Index is populated.
 	var check_if_index_created = function(){
@@ -31,70 +22,117 @@ function Index () {
 	}
 
 	//Function that Creates the Inverted Index from the .json file passed 
-	var loadingData = function(filepath){
+	this.loadingData = function(filepath){
 		//Check if file path is for a .json file
-		if(typeof window !== "undefined"){
-			return fetch('./api/some.json')  
-		  .then(  
-		    function(response) {  
-		      if (response.status !== 200) {  
-		        console.log('Looks like there was a problem. Status Code: ' +  
-		          response.status);  
-		        return;  
-		      }
+		if(typeof window !== "undefined") {
+			return fetch(filepath)
+				.then(
+					function(response) { 
+						if (response.status !== 200) {
+							console.log('Looks like there was a problem. Status Code: ' +  
+								response.status);  
+				       return false;  
+				     }
+				     // Examine the text in the response
 
-		      // Examine the text in the response  
-		      response.json().then(function(data) {  
-		        console.log(data);  
-		      });  
-		    }  
-		  )  
-		  .catch(function(err) {  
-		    console.log('Fetch Error :-S', err);  
-		  });
+				      return response.json();
+				     // .then(function(data) {  
+				     //   return data;  
+				     // });  
+				   })
+				   .catch(function(err) { 
+				   	console.log('Fetch Error :-S', err);  
+				   });
+
+				// return fetch('filepath').then(function(response){
+				// 	return response.json();
+				// }).catch(function(err){
+    //     	console.log('There has been a problem with your fetch operation: ' + err);
+    //     	return false;
+    // });
 		}
-		else{
+		// fetch('filepath')
+		// .then(
+		// 	function(response) { 
+		// 		if (response.status !== 200) {
+		// 			console.log('Looks like there was a problem. Status Code: ' +  
+		// 				response.status);  
+		//        return false;  
+		//      }
+
+		//      // Examine the text in the response  
+		//      response.json().then(function(data) {  
+		//        console.log(data);  
+		//      });  
+		//    })
+		//    .catch(function(err) { 
+		//    	console.log('Fetch Error :-S', err);  
+		//    });
+		// }
+		else {
 			return require(filepath);
-		} 
+		}
+		// }
+		// else{
+		// 	return false;
+		// }
+		
 	}
+	//Function that remove all the stop words above and does the formatting. 
+	var removeStopWords = function(doc_word){
+		var doc = doc_word.toLowerCase().replace(/[,.:]/g, "").split(" ");
+		var doc_without_stop_words = [];
+		for (var i = 0; i < doc.length; i++) {
+			//Checks if the word exists in the stop_words object.
+			if(typeof(stop_words[doc[i]]) === "undefined"){
+				doc_without_stop_words.push(doc[i]);
+			}
+		};
+		return doc_without_stop_words;
+	}
+
 	this.createIndex = function (filepath){
 		//It returns true if the .json file is not empty
 		//returns false when .json is empty.
-		
-
-		//Loadin data from the .json file
-		var loadedData = loadingData(filepath);//use fetch????
-
-		//Function that remove all the stop words above and does the formatting. 
-		var removeStopWords = function(doc_word){
-			var doc = doc_word.toLowerCase().replace(/[,.:]/g, "").split(" ");
-			var doc_without_stop_words = [];
-			for (var i = 0; i < doc.length; i++) {
-				//Checks if the word exists in the stop_words object.
-				if(typeof(stop_words[doc[i]]) === "undefined"){
-					doc_without_stop_words.push(doc[i]);
-				}
-			};
-			return doc_without_stop_words;
+		if (typeof filepath !== 'string') {
+			return "Invalid Arguement";
 		}
-
-        //For loop that creates the Index
-		for (var prop in loadedData){
-			for (var bookProp in loadedData[prop]){
-				var sentence = removeStopWords(loadedData[prop][bookProp]);
-				for(var i = 0; i < (sentence.length); i++){
-					if (inverted_index.hasOwnProperty(sentence[i])){
-						if (inverted_index[sentence[i]].indexOf(prop) == -1) {
-							inverted_index[sentence[i]].push(prop);
+		var loadedData;
+		try {	
+		//Loadin data from the .json file
+			loadedData = this.loadingData(filepath);//use fetch????
+		} catch (e) {
+			console.log(e);
+			return 'Cannot Find ' + filepath;
+		}
+		console.log('gets here');
+		function books(book, index) {
+ 			for (var bookProp in book){
+				var sentence = removeStopWords(book[bookProp]);
+				each(sentence, push);
+				// sentence.forEach(
+				function push(word, count){
+					if (inverted_index.hasOwnProperty(word)){
+						if (inverted_index[word].indexOf(index) == -1) {
+							inverted_index[word].push(index);
 						}
 					}
 					else{
-						inverted_index[sentence[i]] = [prop];
+						inverted_index[word] = [index];
 					} 
-				}
+				};
 			}
+		};
+		if (typeof window !== 'undefined') {
+			return loadedData.then(function (ld) {
+				// console.log(ld);
+				each(ld,books);
+				// console.log(inverted_index);
+				return Promise.resolve(true); 
+			});
+		} else {
+			each(loadedData, books);
 		}
-		return true;
 	};
 	this.getIndex = function (){
 		//Getter Function that returns the inverted index.
@@ -136,9 +174,12 @@ function Index () {
 var index = new Index();
 console.log(index.getIndex());
 console.log(index.searchIndex("ALICE"));
+console.log(index.createIndex("/books"));
+console.log(index.createIndex("/books.json"));
+console.log(index.createIndex());
 index.createIndex("../books.json" );
 console.log(index.searchIndex("ALICE"));
-console.log(index.getIndex());
+// console.log(index.getIndex());
 
-
-module.exports = Index;
+if (typeof module != 'undefined')
+	module.exports = Index;
