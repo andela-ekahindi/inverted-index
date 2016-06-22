c4bc2a4e9b9ecbc70c40a29e0e4fc7cc0a8e2dd8
Esther Dama Kahindi <estherdamakahindi@Esthers-MacBook-Pro.local>
2016-06-22 18:17:50 +0300

    Refactored Code using Peer Reviews

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,19 +1,22 @@
+'use strict';
+
 function Index() {
-  var self = this;
-  //A private variable that will store the Inverted Index
+  //variable that stores
+
+
   this.index = {};
   //Dictonary of Stop words
   this.stopWords = [
-    "a", "i", "an", "and", "as", "at", "by",
-    "for", "has", "in", "is", "it", "of", "on",
-    "that", "the", "to", "was", "were", "with", ''
+    'a', 'i', 'an', 'and', 'as', 'at', 'by',
+    'for', 'has', 'in', 'is', 'it', 'of', 'on',
+    'that', 'the', 'to', 'was', 'were', 'with', ''
   ];
-};
+}
 
 // Prototypes to separate the implementation.
 //Function that checks if the Inverted Index is populated.
 Index.prototype.isCreated = function() {
-  return Object.keys(self.index).length > 0 ? true : false //returns true if the index has values.
+  return Object.keys(this.index).length > 0 ? true : false;
 };
 //Function that Reads the Json data from files. 
 Index.prototype.readJson = function(filepath) {
@@ -31,10 +34,10 @@ Index.prototype.readJson = function(filepath) {
 //Function that remove all the stop words above and does the formatting. 
 Index.prototype.cleanData = function(string) {
   var cleanString = [];
-  string = string.toLowerCase().replace(/[,.:]/g, "").split(" ");
+  string = string.toLowerCase().replace(/[,.:]/g, '').split(' ');
 
-  for (word of string) {
-    if (!self.stopWords.includes(word)) {
+  for (var word of string) {
+    if (!this.stopWords.includes(word)) {
       cleanString.push(word);
     }
   }
@@ -46,14 +49,14 @@ Index.prototype.populateIndex = function(data) {
   data.forEach(function getLoadedData(book, location) {
     var sentences = '';
     Object.keys(book).forEach(function(keys) {
-      sentences += book[keys] + " ";
+      sentences += book[keys] + ' ';
     });
 
     var sentence = self.cleanData(sentences);
 
     sentence.forEach(function(word) {
       if (self.index.hasOwnProperty(word)) {
-        if (self.index[word].indexOf(location) == -1) {
+        if (self.index[word].indexOf(location) === -1) {
           self.index[word].push(location);
         }
       } else {
@@ -64,9 +67,9 @@ Index.prototype.populateIndex = function(data) {
 };
 //Function for creating the Index.
 Index.prototype.createIndex = function(filepath) {
-  self = this;
+  var self = this;
   if (typeof filepath !== 'string') {
-    return "Invalid Arguement";
+    return 'Invalid Arguement';
   } else {
     return this.readJson(filepath).then(function(response) {
         var data = response;
@@ -80,28 +83,28 @@ Index.prototype.createIndex = function(filepath) {
 
 };
 Index.prototype.exist = function(term) {
-  return self.index.hasOwnProperty(term);
-}
+  return this.index.hasOwnProperty(term);
+};
 
 Index.prototype.getIndex = function() {
   //Getter Function that returns the index.
-  return self.isCreated() ? self.index : "No Index Created"
+
+  return this.isCreated() ? this.index : 'No Index Created';
 };
 
 Index.prototype.searchIndex = function(term) {
   //Function that searches the Inverted Index for words.
   //var self = this;
 
-
   if (typeof term !== 'string') {
-    return "Invalid Search Term";
+    return 'Invalid Search Term';
   }
 
-  if (!self.isCreated()) {
-    return "No Index Created";
+  if (!this.isCreated()) {
+    return 'No Index Created';
   }
 
   var input = term.toLowerCase();
   //this is a function that searches for the term in the index. Produces the best results for searching those objects.
-  return self.exist(input) ? self.index[input] : "No such word found"
-}
+  return this.exist(input) ? this.index[input] : 'No such word found';
+};
